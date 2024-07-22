b253fb17b8b068e0fd60d8ca56d557b3077b1d8d
TQT0307 <96462346+TQT0307@users.noreply.github.com>
2024-07-22 12:19:18 +0700

    Update account.dart

--- lib/account/account.dart
+++ lib/account/account.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'OrderHistoryPage.dart';
+
 class Account extends StatefulWidget {
   const Account({super.key});
 
@@ -10,10 +12,51 @@ class Account extends StatefulWidget {
 class _AccountState extends State<Account> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-    body: Center(
-      child: Text('Account'),
-    )
+    List<Map<String, dynamic>> orders = OrderHistory.getOrders(); // Retrieve orders from history
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Account'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(8),
+        children: [
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Edit Profile'),
+            onTap: () {
+              print('Edit Profile Tapped!');
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.history),
+            title: Text('Order History'),
+            onTap: () {
+              // Navigate to a page that displays the order history
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => OrderHistoryPage(),
+              ));
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_cart),
+            title: Text('View Orders'),
+            onTap: () {
+              print('View Orders Tapped!');
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text('Logout'),
+            onTap: () {
+              print('Logout Tapped!');
+            },
+          ),
+        ],
+      ),
     );
   }
 }
+
+
+
